25a9543503174d8380c2d4d168ab4a496231f856
Valentin Speleers <speleerv@frene15.iut-infobio.priv.univ-lille1.fr>
2017-03-24 08:49:42 +0100

    JS tissu

--- server/src/main/webapp/tissu.js
+++ server/src/main/webapp/tissu.js
@@ -8,7 +8,7 @@ function afficher(url){
 }
 
 function afficheTissu(data){
-	var retour;
+	var retour="";
 	for (index = 0; index < data.length; ++index) {
 		retour += "<li>" +data[index].nom + " : <b> longueur : "+data[index].longueur+ " : <b> largeur : "+data[index].largeur +" Type: "+data[index].type+" luminosite :"+data[index].luminosite+"</li>";
 	}
@@ -16,7 +16,7 @@ function afficheTissu(data){
 
 }
 
-function add(nom,long,larg,url){
+function add(nom,long,larg,type,luminosite,url){
 	$.ajax({
 		type : 'POST',
 		contentType : 'application/json',
@@ -26,6 +26,8 @@ function add(nom,long,larg,url){
 			"nom" : nom,
 			"longueur" : long,
 			"largeur" : larg,
+			"type" : type,
+			"luminosite" : luminosite,
 		}),
 		success : function(data, textStatus, jqXHR) {
 			console.log(data);

43d9fc260ba7b1f5b14c9c4bfeec3aff0695668f
Dylan LESAGE <dylan.lesage@etudiant.univ-lille1.fr>
2017-03-27 11:52:51 +0200

    suite bdd

--- server/src/main/webapp/all.js
+++ server/src/main/webapp/all.js
@@ -85,7 +85,7 @@ function postUserGeneric(name, role, email, pwd, url) {
 }
 
 function postFournisseurBdd(nom, foc, adresse, ville,type,statut,diplôme,anneeExperience,heuresSemaine,email,telephone,mdp) {
-    postFournniseurGeneric(nom, foc, statut, diplôme,anneeExperience,heuresSemaine,email,statut,telephone,mdp,"v1/associe/");
+    postFournniseurGeneric(nom, foc, adresse, ville,type,statut,diplôme,anneeExperience,heuresSemaine,email,telephone,mdp,"v1/associe/");
 }
 
 
@@ -107,11 +107,11 @@ function postFournniseurGeneric(nom, foc, adresse, ville,type,statut,diplôme,an
 			"heuresSemaine" : heuresSemaine,
 			"email" : email,
 			"telephone" : telephone,
-			"mdp" : mdp
+			"valide" : 0
 					}),
 		success : function(data, textStatus, jqXHR) {
 			console.log(data);
-			document.location.href="index.html"; 
+			postUserBdd(nom, foc, email, mdp);
 			afficheUser(data);
 		},
 		error : function(jqXHR, textStatus, errorThrown) {
@@ -195,22 +195,23 @@ function afficheOnlyUsers(data) {
 
 
 
-function listOnlyFournisseurBdd() {
-	listOnlyFournisseurGeneric("v1/user/");
+function listOnlyFournisseurBdd(foc) {
+	console.log("ok "+foc);
+	listOnlyFournisseurGeneric("v1/associe/",foc);
 }
 
-function listOnlyFournisseurGeneric(url) {
+function listOnlyFournisseurGeneric(url,foc) {
 	$.getJSON(url, function(data) {
-		afficheOnlyFournisseur(data)
+		afficheOnlyFournisseur(data,foc);
 		});
 }
 
-function afficheOnlyFournisseur(data) {
+function afficheOnlyFournisseur(data,foc) {
 	var html = '<h1>Fournisseur</h1><table><tr><th>Nom</th><th>Email</th><th>Fournis</th></tr>';
 	var js = "<script>$(document).ready(function() {";
 	var index = 0;
 	for (index = 0; index < data.length; ++index) {
-		if(data[index].role=="fournisseur"){
+		if(data[index].foc==foc){
 			html = html + "<tr>";
 			html = html + "<td>"+ data[index].name + "</td>";
 			html = html + "<td>"+ data[index].email + "</td>";
@@ -222,7 +223,7 @@ function afficheOnlyFournisseur(data) {
 					"$.ajax({"+ 
 					 "type : \'DELETE\',"+
 					 "contentType : \'application/json\',"+
-					 "url : \"v1/user/"+data[index].email+"\","+
+					 "url : \"v1/associe/"+data[index].email+"\","+
 					 "dataType : \"json\","+
 					 "data : JSON.stringify({ \"email\" :\""+ data[index].email+"\" }),"+
 					 "success : function(data, textStatus, jqXHR) {"+
@@ -235,8 +236,62 @@ function afficheOnlyFournisseur(data) {
 				"});";
 			}
 		}
-		html = html + "<a href=\"nouveau_compteFournisseur.html\">Inscrire un fournisseur</a>";
 	$("#reponse").html(html+"</table>");
+	js+= "});  </script>";
+	$("#test").html(js);
+}
+
+
+function displayNotif() {
+	$.getJSON("v1/associe/", function(data) {
+		displayNotification(data);
+		});
+}
+
+function displayNotification(data){
+    var html = "<h1>liste des notifications</h1>";
+    var js = "<script>$(document).ready(function() {";
+    html+= "<table><tr><th>nom</th><th>rôle</th><th>adresse</th><th>ville</th><th>type de métier</th><th>statut</th><th>diplôme</th><th>anéée d'expérience</th>";
+    html+= "<th>heure par semaine</th> <th>email</th> <th>telephone</th></tr>";
+   		 for (index = 0; index < data.length; ++index) {
+   			if(data[index].valide==0){
+   				 html+="<tr>";
+    			 html+="<td>"+data[index].nom+"</td>";
+              	 html+="<td>"+data[index].foc+"</td>";
+              	 html+="<td>"+data[index].adresse+"</td>";
+              	 html+="<td>"+data[index].ville+"</td>";
+              	 html+="<td>"+data[index].type+"</td>";
+              	 html+="<td>"+data[index].statut+"</td>";
+              	 html+="<td>"+data[index].diplôme+"</td>";
+              	 html+="<td>"+data[index].anneeExperience+"</td>";
+              	 html+="<td>"+data[index].heuresSemaine+"</td>";
+              	 html+="<td>"+data[index].email+"</td>";
+              	 html+="<td>"+data[index].telephone+"</td></tr>";
+
+
+
+
+              	 js+= "$(\"#delete-user"+index+"\").click(function(){" +
+				"if (confirm(\"Êtes-vous certain de vouloir supprimer l'utilisateur "+data[index].nom+" ?\") == true){"+
+					"$.ajax({"+ 
+					 "type : \'DELETE\',"+
+					 "contentType : \'application/json\',"+
+					 "url : \"v1/associe/"+data[index].email+"\","+
+					 "dataType : \"json\","+
+					 "data : JSON.stringify({ \"email\" :\""+ data[index].nom+"\" }),"+
+					 "success : function(data, textStatus, jqXHR) {"+
+					 "listOnlyFournisseurBdd();"+
+					 "},"+
+					 "error : function(jqXHR, textStatus, errorThrown) {"+
+					 "}"+
+					 "})"+
+				  "}"+
+				"});";
+
+              	}
+              }
+             
+	$("#reponse").html(html);
 	js+= "});  </script>";
 	$("#test").html(js);
 }
